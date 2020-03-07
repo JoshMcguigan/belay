3035b8447d451f67c6c0287f8e7d466dd21ae82f
Josh Mcguigan <joshmcg88@gmail.com>
2020-03-06 19:20:07 -0800

    proper arg parsing

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,65 @@
 use std::{
-    env::{self, current_dir},
+    env::current_dir,
     fs::{read_dir, read_to_string, File, Permissions},
     io::Write,
     os::unix::fs::PermissionsExt,
     path::PathBuf,
     process::Command,
 };
+use structopt::StructOpt;
 
 mod ci;
 use ci::{github::GitHubCiConfig, TaskList};
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
+#[derive(StructOpt)]
+struct Args {
+    #[structopt(subcommand)]
+    subcommand: Option<Subcommand>,
+}
+
+#[derive(StructOpt)]
+enum Subcommand {
+    Hook {
+        #[structopt(subcommand)]
+        hook_type: HookType,
+    },
+}
+
+#[derive(StructOpt)]
+enum HookType {
+    Commit,
+    Push,
+}
+
+impl HookType {
+    fn filename(&self) -> String {
+        match self {
+            HookType::Commit => String::from("pre-commit"),
+            HookType::Push => String::from("pre-push"),
+        }
+    }
+}
+
 fn main() -> Result<()> {
     let root_dir = find_git_root().ok_or("Failed to find git root")?;
 
-    let args = env::args().skip(1).collect::<Vec<String>>();
+    let args = Args::from_args();
 
-    if args.len() == 2 {
+    if let Some(Subcommand::Hook { hook_type }) = args.subcommand {
+        let hook_filename = hook_type.filename();
         let mut hook_path = root_dir;
         hook_path.push(".git");
         hook_path.push("hooks");
-        hook_path.push("pre-push");
+        hook_path.push(&hook_filename);
 
         let mut file = File::create(&hook_path)?;
         file.set_permissions(Permissions::from_mode(0o755))?;
 
         file.write_all(b"#!/usr/bin/sh\nbelay")?;
 
-        println!("Created hook `.git/hooks/pre-push`");
+        println!("Created hook `.git/hooks/{}`", hook_filename);
 
         return Ok(());
     }
