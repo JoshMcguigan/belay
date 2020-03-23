355bb78223b6fe93c4b82e22c5589c95ee914e65
Josh Mcguigan <joshmcg88@gmail.com>
2020-03-22 19:25:29 -0700

    read config from disk

--- src/config.rs
+++ src/config.rs
@@ -1,11 +1,39 @@
+use std::fs;
+
+use directories::ProjectDirs;
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize)]
 pub struct Config {
     pub command_blacklist: Vec<String>,
 }
 
 impl Config {
     pub fn read() -> Self {
-        // TODO read from file system, if it doesn't exist write default to file system
-        Self::default()
+        let project_dirs =
+            ProjectDirs::from("com", "cargo", "belay").expect("failed to find home directory");
+        let config_path = project_dirs.config_dir().join("config.yml");
+
+        if config_path.is_file() {
+            serde_yaml::from_str::<Config>(
+                &fs::read_to_string(config_path).expect("failed to read config file"),
+            )
+            .expect("failed to read config file as yaml")
+        } else {
+            fs::create_dir_all(config_path.parent().expect("config dir should have parent"))
+                .expect("failed to create config directory");
+            let config = Self::default();
+
+            let config_as_string =
+                serde_yaml::to_string(&config).expect("failed to convert default config to string");
+
+            fs::File::create(&config_path).expect("failed to create config file");
+
+            fs::write(&config_path, config_as_string.as_bytes())
+                .expect("failed to write config to file");
+
+            config
+        }
     }
 
     /// Creates a default configuration.
